7119e0d5138d59b06aef6bbefc0086517b88f983
kanboakeaw <kanboakeaw@gmail.com>
2019-05-02 10:37:33 +0700

    Get user list

--- routes/user.js
+++ routes/user.js
@@ -10,43 +10,39 @@ router.get("/", (req, res) => {
 
 router.get('/list/:pageNumber', (req, res) => {
     const pageNumber = req.params.pageNumber;
-    console.log(pageNumber);
-
-    MongoClient.connect(
-        "mongodb+srv://weerayut:22374736@cluster0-4wunc.gcp.mongodb.net/newDatabase62?retryWrites=true", {
-            useNewUrlParser: true
-        },
-        function (err, db) {
+    let itemPerPage = 10;
+    MongoClient.connect("mongodb+srv://weerayut:22374736@cluster0-4wunc.gcp.mongodb.net/newDatabase62?retryWrites=true", {
+        useNewUrlParser: true
+    }, function (err, db) {
+        if (err) {
+            res.sendStatus(404);
+            return;
+        }
+        let dbo = db.db("newDatabase62");
+        dbo.collection('userLoginTable').find({}, {
+            limit: itemPerPage,
+            skip: itemPerPage * (Number(pageNumber) - 1),
+            projection: {
+                _id: 1,
+                rank: 1,
+                first_name: 1,
+                last_name: 1,
+                id_mil: 1,
+                unit_name: 1,
+                username: 1,
+                password: 0
+            }
+        }).sort({
+            _id: -1
+        }).toArray(function (err, result) {
             if (err) {
                 res.sendStatus(404);
-                return;
+            } else {
+                res.send(result);
             }
-            let dbo = db.db("newDatabase62");
-            dbo
-                .collection('userLoginTable')
-                .find({}, {
-                    limit: 10,
-                    skip: 10 * (Number(pageNumber) - 1),
-                    projection: { //ต้องการให้รับข้อมูล
-                        _id: 0,
-                        first_name: 1, // 1 แสดง 0 ไม่แสดง
-                        last_name: 1,
-                        username: 1
-                    }
-                })
-                .sort({
-                    _id: -1
-                })
-                .toArray(function (err, result) {
-                    if (err) {
-                        res.sendStatus(404);
-                    } else {
-                        res.send(result);
-                    }
-                    db.close();
-                });
-        }
-    );
+            db.close();
+        });
+    });
 });
 
 module.exports = router;
