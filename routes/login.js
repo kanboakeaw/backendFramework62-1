f9f88695d04ea5f88e984ca737687422de1a67c7
kanboakeaw <kanboakeaw@gmail.com>
2019-05-02 10:29:18 +0700

    Add generate jwt

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const MongoClient = require("mongodb").MongoClient;
+const jwt = require("jsonwebtoken");
 
 router.get("/", (req, res) => {
     res.send({
@@ -64,11 +65,11 @@ router.post("/register", (req, res) => {
             }, function (err, result) {
                 if (err) {
                     res.send({
-                        status: false
+                        status: false,
+                        message: err.message
                     });
                 }
                 console.log(result);
-
                 if (result) {
                     res.send({
                         status: false,
@@ -78,12 +79,12 @@ router.post("/register", (req, res) => {
                     dbo.collection("userLoginTable").insertOne(userObj, function (err, result) {
                         if (err) {
                             res.send({
-                                status: false
+                                status: false,
+                                message: err.message
                             });
                         }
                         res.send({
-                            status: "store success",
-                            name: nameVar //response
+                            status: true
                         });
                     });
                 }
@@ -92,7 +93,6 @@ router.post("/register", (req, res) => {
         }
     );
 });
-
 
 router.post("/login", (req, res) => {
     var nameVar = req.body.username; // name คือตัวที่พิมลง postman
@@ -110,18 +110,31 @@ router.post("/login", (req, res) => {
             dbo.collection("userLoginTable").findOne({
                 username: nameVar,
                 password: passwordVar
-            }, function (err, result) {
-                console.log(result);
-
+            }, function (err, user) {
                 if (err) {
                     res.send({
                         status: false
                     });
                 }
-                if (result) {
-                    res.send({
-                        status: true
+                if (user) {
+                    var jwtBearerToken = jwt.sign({
+                        id: user._id,
+                        rank: user.rank,
+                        first_name: user.first_name,
+                        last_name: user.last_name,
+                        id_mil: user.id_mil,
+                        unit_name: user.unit_name,
+                        username: user.username
+                    }, 'secret', {
+                        expiresIn: "365d", // 1 Year
+                        subject: "JWT"
                     });
+                    res.send(
+                        JSON.stringify({
+                            status: true,
+                            token: jwtBearerToken
+                        })
+                    );
                 } else {
                     res.send({
                         status: false
@@ -129,6 +142,27 @@ router.post("/login", (req, res) => {
                 }
             });
             db.close();
+        }
+    );
+});
+router.post('/verifyToken', (req, res) => {
+    jwt.verify(
+        req.body.token,
+        'secret', {},
+        function (err, payload) {
+            if (payload) {
+                res.send(
+                    JSON.stringify({
+                        verify: true
+                    })
+                );
+            } else {
+                res.send(
+                    JSON.stringify({
+                        verify: false
+                    })
+                );
+            }
         }
     );
 });
